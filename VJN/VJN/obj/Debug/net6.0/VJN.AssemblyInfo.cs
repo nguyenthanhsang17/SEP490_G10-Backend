2992736e90235dbdf1664a0246a657dc414bb8eb
nguyenthanhsang17 <nguyenthanhsang17102002@gmail.com>
2024-11-13 16:55:51 +0700

    profile fix bug

--- VJN/VJN/obj/Debug/net6.0/VJN.AssemblyInfo.cs
+++ VJN/VJN/obj/Debug/net6.0/VJN.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("VJN")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cf7459f2a766b1f63ff58d5dcbd19bb98046f125")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+64f0fa9adb504b95c6420c0bc2f8106657ec5c3e")]
 [assembly: System.Reflection.AssemblyProductAttribute("VJN")]
 [assembly: System.Reflection.AssemblyTitleAttribute("VJN")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
