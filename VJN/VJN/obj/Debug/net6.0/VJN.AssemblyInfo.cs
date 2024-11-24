537e22828d6ed7fa40621405e6cd39c7582ff13d
nguyenthanhsang17 <nguyenthanhsang17102002@gmail.com>
2024-11-24 23:15:47 +0700

    ....

--- VJN/VJN/obj/Debug/net6.0/VJN.AssemblyInfo.cs
+++ VJN/VJN/obj/Debug/net6.0/VJN.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("VJN")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1c8f61992de143b117ec22182bbb339f1c9fa4bc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("VJN")]
 [assembly: System.Reflection.AssemblyTitleAttribute("VJN")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
