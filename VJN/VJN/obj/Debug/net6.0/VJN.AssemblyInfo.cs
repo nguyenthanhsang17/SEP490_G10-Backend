2b8775930197cff73e73112433e35d4428597f30
nguyenthanhsang17 <nguyenthanhsang17102002@gmail.com>
2024-12-05 22:29:16 +0700

    Sang fix code

--- VJN/VJN/obj/Debug/net6.0/VJN.AssemblyInfo.cs
+++ VJN/VJN/obj/Debug/net6.0/VJN.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("VJN")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+192a9c04ec8ff274c596683fc0ce54e47826573b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+20e9c01cf73de5658b873186b39e7e27d566ff21")]
 [assembly: System.Reflection.AssemblyProductAttribute("VJN")]
 [assembly: System.Reflection.AssemblyTitleAttribute("VJN")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
